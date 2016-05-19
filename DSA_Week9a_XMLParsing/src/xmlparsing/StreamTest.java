0f4ea2f9fb21301a5181bc74b4335fc7a3dd03a7
Naooomi <naomi.wuuu@gmail.com>
2016-05-19 21:27:33 +1200

    DataOutputStream____Messssssy code

--- DSA_Week9a_XMLParsing/src/xmlparsing/StreamTest.java
+++ DSA_Week9a_XMLParsing/src/xmlparsing/StreamTest.java
@@ -2,6 +2,7 @@ package xmlparsing;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -68,28 +69,29 @@ public class StreamTest
     {
         ArrayList<Person> list = new ArrayList<Person>();
         list.add(new Person("111", 19, 120.5f));
-        list.add(new Person("222", 45, 150.3f));
-        list.add(new Person("333", 63, 160.3f));
-        list.add(new Person("444", 12, 80.2f));
-        list.add(new Person("555", 37, 110.9f));
-        list.add(new Person("666", 28, 108.4f));
-        list.add(new Person("777", 23, 103.5f));
-        list.add(new Person("888", 26, 105.0f));
-        list.add(new Person("999", 25, 99.3f));
-        list.add(new Person("101010", 27, 115.9f));
+//        list.add(new Person("222", 45, 150.3f));
+//        list.add(new Person("333", 63, 160.3f));
+//        list.add(new Person("444", 12, 80.2f));
+//        list.add(new Person("555", 37, 110.9f));
+//        list.add(new Person("666", 28, 108.4f));
+//        list.add(new Person("777", 23, 103.5f));
+//        list.add(new Person("888", 26, 105.0f));
+//        list.add(new Person("999", 25, 99.3f));
+//        list.add(new Person("101010", 27, 115.9f));
         
         try 
         {
             //obtain output stream to file and layer with data output stream
-            DataOutputStream dos = new DataOutputStream(new FileOutputStream("peopleOutput.dat"));
+            File outputFile = new File("peopleOutput.txt");
+            DataOutputStream dos = new DataOutputStream(new FileOutputStream(outputFile));
             //firstly write to file how many person objects in collection
             dos.writeInt(list.size());
             for(Person person:list)
             {
                 //write the name as UTF string, int for age and float for weight to file
                 dos.writeUTF(person.getName());
-                dos.writeInt(person.getAge());
-                dos.writeFloat(person.getWeight());
+                dos.writeInt((int)person.getAge());
+                dos.writeFloat((float)person.getWeight());
             }
             dos.flush();
             dos.close();
@@ -99,39 +101,29 @@ public class StreamTest
             System.out.println("Data output stream problem writing to data file");
         }
         
-        ArrayList<Person> personList = new ArrayList<Person>();
-        try 
-        {
-            //obtain input stream to file and layer with a data input stream
-            DataInputStream dis = new DataInputStream(new FileInputStream("peopleOutput.dat"));
-            //read in first the amount of Person objects held in file
-            int numElements = dis.readInt();
-            //perform a loop reading in each Person object(protocol of file must match)
-            for(int i=0; i<numElements; i++)
-            {
-                //read in name(UTF string), age(int) and weight(float) from file stream
-                //Order is very important otherwise data gets corrupted.
-                //Create Person Object
-                Person p = new Person(dis.readUTF(), dis.readInt(), dis.readFloat());
-                personList.add(p);
-                System.out.println(personList);
-            }
-        } 
-        catch (IOException e) 
-        {
-            System.out.println("Data input stream problem writing to data file");
-        }
+//        ArrayList<Person> personList = new ArrayList<Person>();
+//        try 
+//        {
+//            //obtain input stream to file and layer with a data input stream
+//            DataInputStream dis = new DataInputStream(new FileInputStream("peopleOutput.txt"));
+//            //read in first the amount of Person objects held in file
+//            int numElements = dis.readInt();
+//            //perform a loop reading in each Person object(protocol of file must match)
+//            for(int i=0; i<numElements; i++)
+//            {
+//                //read in name(UTF string), age(int) and weight(float) from file stream
+//                //Order is very important otherwise data gets corrupted.
+//                //Create Person Object
+//                Person p = new Person(dis.readUTF(), dis.readInt(), dis.readFloat());
+//                personList.add(p);
+//                System.out.println(personList);
+//            }
+//        } 
+//        catch (IOException e) 
+//        {
+//            System.out.println("Data input stream problem writing to data file");
+//        }
         
     }
-   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+ 
 }
