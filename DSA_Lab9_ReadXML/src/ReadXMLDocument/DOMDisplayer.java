8b0aed6b27272052723bf919c05bde6ea43cbf5b
Naooomi <naomi.wuuu@gmail.com>
2016-05-24 12:07:49 +1200

    ...

--- DSA_Lab9_ReadXML/src/ReadXMLDocument/DOMDisplayer.java
+++ DSA_Lab9_ReadXML/src/ReadXMLDocument/DOMDisplayer.java
@@ -10,6 +10,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.InputStream;
+import java.io.OutputStream;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,9 +18,18 @@ import javax.swing.SwingConstants;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Comment;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 
 /**
  *
@@ -106,7 +116,44 @@ public class DOMDisplayer extends JPanel implements ActionListener
             }
         }
             
-          
+        public void writeXMLToStream(OutputStream os) throws TransformerConfigurationException, TransformerException
+        {
+            if(document!=null)
+            {
+                TransformerFactory transFactory = TransformerFactory.newInstance();
+                Transformer transformer = transFactory.newTransformer();
+                transformer.transform(new DOMSource(document), new StreamResult(os));
+            }
+        }
+        
+        //recursive method that adds one new tree node to the
+        //parentTreeNode for each child element of parentXMLNode
+        private void addElementChildrenToTree(Node parentXMLNode, DefaultMutableTreeNode parentTreeNode)
+        {
+            NodeList childNodes = parentXMLNode.getChildNodes();
+            int numChildNodes = childNodes.getLength();
+            for(int i=0; i<numChildNodes; i++)
+            {
+                Node xmlNode = childNodes.item(i);
+                String label;
+                if(xmlNode instanceof Text || xmlNode instanceof Comment)
+                {
+                    //just give the text or comment trimmed of whitespace
+                    label = xmlNode.getNodeValue().trim();
+                }
+                else
+                    label = getNodeLabel(xmlNode);
+                
+                if(label!=null && label.length()>0)
+                {
+                    //add this label as a tree node
+                    DefaultMutableTreeNode treeNode = new DefaultMutableTreeNode(label);
+                    parentTreeNode.add(treeNode);
+                    addElementChildrenToTree(xmlNode, treeNode);
+                }                
+            }
+        }
+        
         //helper method that prepares a string description of an XMLNode
         //consisting of the node name followed by its attributes
         //shown in brackets
@@ -117,9 +164,16 @@ public class DOMDisplayer extends JPanel implements ActionListener
             
             if(nodeAttributes!=null && nodeAttributes.getLength()>0)
             {
-                
-                
-                
+                label+=" (";
+                int numAttributes = nodeAttributes.getLength();
+                for(int i=0; i<numAttributes; i++)
+                {
+                    Node attribute = nodeAttributes.item(i);
+                    if(i > 0)
+                        label += ", ";
+                    label += attribute.getNodeName() + "=" + attribute.getNodeValue();
+                }
+                label += ")";
             }
             return label;
         }  
